7d40c171c8865ac5026fd10c455a938f6c6989bd
19013na <19013@naver.com>
2024-09-14 22:33:08 +0900

    Chore : picNFace 예외처리

--- picNface.py
+++ picNface.py
@@ -61,17 +61,15 @@ def process_images(license_image_path, face_image_path):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     
+    # 신분증 사진에서 얼굴을 찾지 못했을 때
     face_locations = face_recognition.face_locations(licenseimg)
-    print("Face locations:", face_locations)
+    if len(face_locations) == 0:
+        return "No face found in license image", "license no face"
 
     faceLoc = face_recognition.face_locations(licenseimg)[0]
-    
     encodeRef = face_recognition.face_encodings(licenseimg)[0]
-    cv2.rectangle(licenseimg, (faceLoc[3], faceLoc[0]), (faceLoc[1], faceLoc[2]), (255, 0, 255), 2)
 
-    if use_camera:
-        # 카메라 캡처 시작
-        cap = cv2.VideoCapture(0)
+    cv2.rectangle(licenseimg, (faceLoc[3], faceLoc[0]), (faceLoc[1], faceLoc[2]), (255, 0, 255), 2)
 
     # CSV 파일 존재 여부 확인 및 초기화 : 본인 경로로 재설정 필요
     csv_filename = 'C:/Users/SAMSUNG/Desktop/detection/Detection/face_rec_data/face_features.csv'
@@ -83,20 +81,18 @@ def process_images(license_image_path, face_image_path):
     start_time = time.time()
     best_face_encoding = None
     best_min_distance = float('inf')
+    best_match_text = None
 
     while True:
-        if use_camera:
-            ret, frame = cap.read()
-            if not ret:
-                break
-            # 프레임을 RGB로 변환
-            rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        else:
-            rgb_frame = imgTest
+        rgb_frame = imgTest
 
         # 프레임에서 얼굴 인식 및 인코딩
         faceLocTest = face_recognition.face_locations(rgb_frame)
         encodeTest = face_recognition.face_encodings(rgb_frame, faceLocTest)
+
+        # 테스트 이미지에서 얼굴을 찾지 못했을 때
+        if len(encodeTest) == 0:
+            return "No face found in test image", "face no face"
 
         for (top, right, bottom, left), face_encoding in zip(faceLocTest, encodeTest):
             # 기준 얼굴과 현재 프레임 얼굴 비교
@@ -110,22 +106,16 @@ def process_images(license_image_path, face_image_path):
                 best_match_text = f'{results[0]} {round(best_min_distance, 2)}'
 
             # 얼굴 주위에 사각형 그리기
-            if use_camera:
-                cv2.rectangle(frame, (left, top), (right, bottom), (255, 0, 255), 2)
-            else:
-                cv2.rectangle(imgTest, (left, top), (right, bottom), (255, 0, 255), 2)
+            cv2.rectangle(imgTest, (left, top), (right, bottom), (255, 0, 255), 2)
 
         # 결과와 유사성을 이미지 상단에 명시
         if best_match_text:
-            if use_camera:
-                cv2.putText(frame, best_match_text, (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
-            else:
-                cv2.putText(imgTest, best_match_text, (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
+            cv2.putText(imgTest, best_match_text, (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
 
         # N초가 지나면 유사도 값 저장(현재는 10초로 설정)
         if time.time() - start_time >= 10:
             if best_face_encoding is not None:
-                if best_min_distance <= 0.60:#유사도 N이하면 얼굴 데이터 csv로 저장(현재는 0.6로 설정)
+                if best_min_distance <= 0.50:#유사도 N이하면 얼굴 데이터 csv로 저장(현재는 0.5로 설정)
                     print(f"Face recognized with distance: {best_min_distance}. Features saved.")
                     # CSV 파일에 특징값 저장
                     df = pd.read_csv(csv_filename)
@@ -135,25 +125,18 @@ def process_images(license_image_path, face_image_path):
                         df.to_csv(csv_filename, index=False)
                 else:
                     print("동일인이 아니라고 판별되어 얼굴 데이터를 저장하지 않았습니다.")
-                    return False;
+                    return "Not the same person", "not same";
             break
 
         # 결과 보여주기
-        if use_camera:
-            cv2.imshow('Camera', frame)
-        else:
-            cv2.imshow('Test Image', imgTest)
-
+        cv2.imshow('Test Image', imgTest)
         cv2.imshow('Reference Face', licenseimg)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
     # 카메라 및 윈도우 종료
-    if use_camera:
-        cap.release()
-    cv2.destroyAllWindows()
-    return csv_filename
+    return csv_filename, True
 
 
 
@@ -163,8 +146,8 @@ def upload_to_s3(user_id, file_name, bucket, object_name=None):
     # AWS S3 클라이언트 생성
     s3_client = s3_connection(AWS_ACCESS_KEY, AWS_SECRET_KEY)  
 
-    #if s3_client is False :
-        # 동일인이 아닐 경우라고 판단하는 경우 그냥 특정 오류를 발생하고 싶음
+    if s3_client is False :
+        return "AWS S3 connection failed", "not connect"
     try:
         # 파일 업로드
         s3_client.upload_file(file_name, bucket, object_name or file_name)
@@ -175,15 +158,21 @@ def upload_to_s3(user_id, file_name, bucket, object_name=None):
         
         s3_url = f"https://{bucket}.s3.{S3_REGION_NAME}.amazonaws.com/{object_name}"
         print(f"File URL: {s3_url}")        
-        return s3_url
+        return s3_url, "success"
     except FileNotFoundError:
         print(f"The file '{file_name}' was not found.")
-        return False
+        return "File not found", "not find"
     except NoCredentialsError:
         print("AWS credentials not available.")
-        return False
+        return "AWS credentials not available", "not credential"
 
-
+def error_request(user_id, samePerson):
+    flask_url = "http://localhost:5000/send-identity"
+    final_data = {
+        "userId" : user_id,
+        "identity" : "",
+        "samePerson" : samePerson
+    }
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
@@ -195,7 +184,13 @@ if __name__ == "__main__":
     user_id = sys.argv[3]    
 
     # 이미지 처리 및 CSV 파일 생성
-    csv_file_path = process_images(license_image_path, face_image_path)
+    csv_file_path, process_success = process_images(license_image_path, face_image_path)
+
+    if not process_success:
+        print(f"이미지 처리 실패: {csv_file_path}")
+        error_request(user_id, process_success)
+        sys.exit(1)
+
     print(f"CSV 파일이 {csv_file_path}에 저장되었습니다.")
 
     # S3 버킷 이름 설정
@@ -205,20 +200,26 @@ if __name__ == "__main__":
     # 저장할 이름 형식 : face/userId_UUID
     random_uuid = uuid.uuid4()
     s3_object_name = f"face/user{user_id}_{random_uuid}.csv"
-    s3_url = upload_to_s3(user_id, csv_file_path, bucket_name, s3_object_name)
+    s3_url, process_success = upload_to_s3(user_id, csv_file_path, bucket_name, s3_object_name)
+
+    if not process_success:
+        print(f"S3 업로드 실패: {s3_url}")
+        error_request(user_id, process_success)
+        sys.exit(1)
+
+    # CSV 파일 삭제
+    os.remove(csv_file_path)
+    print(f"로컬 CSV 파일 '{csv_file_path}'이 삭제되었습니다.")
 
     flask_url = "http://localhost:5000/send-identity"
-    if not s3_url:
-        print("S3 업로드 중 오류가 발생했습니다.")
-    else :
-        os.remove(csv_file_path)
-        print(f"로컬 CSV 파일 '{csv_file_path}'이 삭제되었습니다.")
-        final_data = {
-            "userId" : user_id,
-            "identity" : s3_url
-        }
-        try:
-            response = requests.post(flask_url, json=final_data)
-            print("Flask 서버로 데이터 전송 완료:", response.status_code)
-        except requests.RequestException as e:
-            print("Flask 서버로의 요청 중 오류 발생:", e)
+    final_data = {
+        "userId" : user_id,
+        "identity" : s3_url,
+        "samePerson" : process_success
+    }
+    
+    try:
+        response = requests.post(flask_url, json=final_data)
+        print("Flask 서버로 데이터 전송 완료:", response.status_code)
+    except requests.RequestException as e:
+        print("Flask 서버로의 요청 중 오류 발생:", e)
